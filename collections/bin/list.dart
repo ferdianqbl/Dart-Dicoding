824ee00ff3a9380d27962098232de161c22fb672
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-04-02 13:21:00 +0700

    add list

--- collections/bin/list.dart
+++ collections/bin/list.dart
@@ -3,4 +3,40 @@ void main(List<String> args) {
   List dynamicList = [1, 'Dicoding', true]; // List<dynamic>
   numberList.forEach((number) => print(number));
   dynamicList.forEach((element) => print(element));
+
+  dynamicList.add('Flutter');
+  dynamicList.forEach((element) => print(element));
+
+  dynamicList.insert(0, 'Programming');
+  dynamicList.forEach((element) => print(element));
+
+  // dynamicList.remove('Programming'); // Menghapus list dengan nilai Programming
+  // dynamicList.removeAt(1); // Menghapus list pada index ke-1
+  // dynamicList.removeLast(); // Menghapus data list terakhir
+  // dynamicList.removeRange(0, 2);
+  // Menghapus list mulai index ke-0 sampai ke-1 (indeks 2 masih dipertahankan)
+
+  // dynamicList.clear(); // Menghapus semua data list
+  // dynamicList.isEmpty; // Menghapus semua data list
+  // dynamicList.isNotEmpty; // Menghapus semua data list
+  // dynamicList.length; // Menghapus semua data list
+  // dynamicList.reversed; // Menghapus semua data list
+  // dynamicList.sort(); // Menghapus semua data list
+  // dynamicList.shuffle(); // Menghapus semua data list
+  // dynamicList.sublist(0, 2); // Menghapus semua data list
+  // dynamicList.toString(); // Menghapus semua data list
+  // dynamicList.toList(); // Menghapus semua data list
+  // dynamicList.toSet(); // Menghapus semua data list
+  // dynamicList.contains('Dicoding'); // Menghapus semua data list
+  // dynamicList.indexOf('Dicoding'); // Menghapus semua data list
+  // dynamicList.lastIndexOf('Dicoding'); // Menghapus semua data list
+
+  var favorites = ['Seafood', 'Salad', 'Nugget', 'Soup'];
+  var others = ['Cake', 'Pie', 'Donut'];
+  var allFavorites = [...favorites, ...others];
+  print(allFavorites);
+
+/* output
+[Seafood, Salad, Nugget, Soup, Cake, Pie, Donut]
+ */
 }
